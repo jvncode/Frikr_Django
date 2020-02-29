e966a2d5182da596342224ec0f77bda407d1aa7c
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-02-29 18:46:13 +0100

    Creación de templates y ajuste de controladores

--- photos/models.py
+++ photos/models.py
@@ -18,4 +18,8 @@ class Photo(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)
 
+    def __str__(self):
+        return self.name
+
+
 
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from photos.models import Photo
 
 def home(request):
-    return HttpResponse("Hola mundo!")
+    photos = Photo.objects.all().order_by('-created_at') #Con order_by hacemos que ordene la consulta con la fecha de creacion
+    context = {
+        'photos_list': photos[:5]
+    }
+    return render(request, 'photos/home.html', context)
