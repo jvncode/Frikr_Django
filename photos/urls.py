f384cf2363679dc709acd7d81485167fd85063ad
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-03-03 02:50:11 +0100

    Implementación y autenticación de vistas genéricas, archivos estáticos e incoporación de Bootsión de Bootstrap

--- photos/urls.py
+++ photos/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from photos.views import HomeView, DetailView, CreateView, ListView
+from photos.views import HomeView, DetailView, CreateView, PhotoListView, UserPhotosView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', HomeView.as_view(), name='photos_home'),
-    path('photos/', ListView.as_view(), name='photos_list'),
+    path('my-photos/', login_required(UserPhotosView.as_view()), name='user_photos'),
+    path('photos/', PhotoListView.as_view(), name='photos_list'),
     path('photos/(?P<pk>[0-9]+)', DetailView.as_view(), name='photo_detail'),
     path('photos/new_photo', CreateView.as_view(), name='create_photo')
 ]
--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,7 @@ from photos.forms import PhotoForm
 from photos.models import Photo, PUBLIC
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.views.generic import View
+from django.views.generic import View, ListView
 from django.utils.decorators import method_decorator
 from django.db.models import Q
 
@@ -98,7 +98,7 @@ class CreateView(View):
         }
         return render(request, 'photos/new_photo.html', context)
 
-class ListView(View, PhotosQueryset):
+class PhotoListView(View, PhotosQueryset):
     def get(self, request):
         """
         Devuelve:
@@ -112,4 +112,12 @@ class ListView(View, PhotosQueryset):
             'photos': self.get_photos_queryset(request)
         }
         return render(request, 'photos/photos_list.html', context)
+
+class UserPhotosView(ListView):
+    model = Photo
+    template_name = 'photos/user_photos.html'
+    
+    def get_queryset(self):
+        queryset = super(UserPhotosView, self).get_queryset()
+        return queryset.filter(owner=self.request.user)
 
