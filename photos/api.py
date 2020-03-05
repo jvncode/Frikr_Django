8f8e6699b7593720ff3dcbf73f19b4ed52231d29
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-03-05 02:13:49 +0100

    Implementación de filtrado, ordenación y búsqueda de elementos en API

--- photos/api.py
+++ photos/api.py
@@ -4,12 +4,19 @@ from photos.models import Photo
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from photos.views import PhotosQueryset
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-
+"""
+Este ViewSet hace lo mismo que las clases PhotoListAPI y PhotoDetailAPI
+pero en una sola clase
+"""
 
 class PhotoViewSet(PhotosQueryset, ModelViewSet):
     queryset = Photo.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    filter_backends = (SearchFilter, OrderingFilter) # Esta linea, la 18 y 19, establecen metodos de búsqueda y orden de las entradas desde la API
+    search_fields = ('name', 'description') # Esta por 'search' (búsqueda)
+    ordering_fields = ('name', 'owner') # Esta por 'ordering' (orden)
 
     def get_queryset(self):
         return self.get_photos_queryset(self.request)
